069a78d2fde4718cfd24057ee30c7af238e2c0b1
Adam Klingbaum <adam.klingbaum@gmail.com>
2022-01-18 21:48:06 -0500

    Implement error handlers / validators for items

---
+++ server/errors/items.js
@@ -0,0 +1,68 @@
+const { Item } = require('../models');
+const ApiError = require('../middleware');
+const validateStringField = require('./validateStringField');
+
+const validateMinStock = (minStock) => {
+  if (minStock === undefined) {
+    return ApiError.badRequest('minStock is required');
+  }
+
+  if (typeof minStock !== 'number') {
+    return ApiError.badRequest('minStock must be an integer');
+  }
+
+  if (minStock < 0) {
+    return ApiError.badRequest('minStock must be at least 0');
+  }
+
+  return null;
+};
+
+const validateItemFields = (name, minStock, vendorName) => {
+  const nameError = validateStringField('name', name);
+  if (nameError) {
+    return nameError;
+  }
+
+  const minStockError = validateMinStock(minStock);
+  if (minStockError) {
+    return minStockError;
+  }
+
+  const vendorNameError = validateStringField('vendorName', vendorName);
+  if (vendorNameError) {
+    return vendorNameError;
+  }
+
+  return null;
+};
+
+module.exports.createItem = (name, minStock, vendorName) => {
+  const fieldValidationError = validateItemFields(name, minStock, vendorName);
+  return fieldValidationError;
+};
+
+module.exports.updateItemById = async (itemId, name, minStock, vendorName) => {
+  const fieldValidationError = validateItemFields(name, minStock, vendorName);
+  if (fieldValidationError) {
+    return fieldValidationError;
+  }
+
+  const item = await Item.findByPk(itemId);
+  if (!item) {
+    return ApiError.notFound('item not found');
+  }
+  return null;
+};
+
+module.exports.getAllItems = () => null;
+
+module.exports.getItemById = () => null;
+
+module.exports.deleteItemById = async (itemId) => {
+  const item = await Item.findByPk(itemId);
+  if (!item) {
+    return ApiError.notFound('item not found');
+  }
+  return null;
+};
